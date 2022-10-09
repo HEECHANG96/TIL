e7f9a7e4af2ada85c08bbaf58f1e26cd84ddba9c
Heechang Jeong <70733630+HEECHANG96@users.noreply.github.com>
2022-10-09 20:08:48 +0900

    Add files via upload

---
+++ 일기장/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+import { useRef, useState } from 'react';
+
+
+const App = () => {
+
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    console.log(newDiaryList);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+        data.map((it) => it.id === targetId ? {...it, content: newContent} : it
+        )
+    );
+  };
+
+  return (
+    <div className="App">
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onRemove={onRemove} diaryList={data} onEdit={onEdit} />
+    </div>
+  );
+}
+
+export default App;
